5fee090522f5e680f4c5c7b15c81736a0838a5da
Chi1989 <86145394+Chi1989@users.noreply.github.com>
2022-05-06 01:07:09 +0100

    Add files via upload
    This is a Deca Meal Onboarding Screen.

---
+++ MySplashScreen/MySplashScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySplashScreen
+//
+//  Created by Decagon on 05/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
